452befcc2a22a6651a1bdc6d2fb915e30c6a2c6a
Ryan Harmon <ryan@bloomcu.com>
2024-07-29 11:10:35 -0600

    Clean up nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-
-  // ssr: true, 
+  
   hooks: {
     'pages:extend': (pages) => {
       pages.push({
