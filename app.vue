dc4b004ac12276c76f2cbb07ceeaae3a3a028722
Ryan Harmon <ryan@bloomcu.com>
2024-08-03 21:05:56 -0600

    Cleanup app.vue

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
       <NuxtPage />
     </NuxtLayout>
 
-    <component :is="`style`">
+    <!-- <component :is="`style`">
       :root {
         --primary: {{ primary }};
         --secondary: {{ secondary }};
@@ -16,31 +16,6 @@
         --black: {{ black }};
         --grey: {{ grey }};
         --white: {{ white }};
-      }
-    </component>
-
-    <!-- Setup primary, secondary and accent color css variables -->
-    <!-- <component :is="`style`">
-      :root {
-          --primary: {{ primary }};
-          --secondary: {{ secondary }};
-          --tertiary: {{ tertiary }};
-          --neutral: {{ neutral }};
-          --black: {{ black }};
-          --grey: {{ grey }};
-          --white: {{ white }};
-      }
-
-      :root {
-        --wrapper-bg: var(--white);
-        --pretitle-color: var(--secondary);
-        --title-color: var(--primary);
-        --body-color: var(--primary);
-        --btn-primary-bg: var(--secondary);
-        --btn-primary-text: var(--white);
-        --btn-secondary-bg: var(--primary);
-        --btn-secondary-text: var(--white);
-        --btn-simple-text: var(--primary);
       }
 
       [data-theme='theme-1'] {
@@ -54,30 +29,6 @@
         --btn-secondary-text: var(--white);
         --btn-simple-text: var(--primary);
       }
-
-      [data-theme='theme-2'] {
-        --wrapper-bg: var(--primary);
-        --pretitle-color: var(--white);
-        --title-color: var(--white);
-        --body-color: var(--white);
-        --btn-primary-bg: var(--secondary);
-        --btn-primary-text: var(--white);
-        --btn-secondary-bg: var(--white);
-        --btn-secondary-text: var(--black);
-        --btn-simple-text: var(--white);
-      }
-
-      [data-theme='theme-3'] {
-        --wrapper-bg: var(--secondary);
-        --pretitle-color: var(--white);
-        --title-color: var(--white);
-        --body-color: var(--white);
-        --btn-primary-bg: var(--white);
-        --btn-primary-text: var(--primary);
-        --btn-secondary-bg: var(--white);
-        --btn-secondary-text: var(--primary);
-        --btn-simple-text: var(--white);
-      }
     </component> -->
   </div>
 </template>
@@ -90,16 +41,6 @@ const neutral = 'green'
 const black = '#000000'
 const grey = '#808080'
 const white = '#ffffff'
-
-// const colors = {
-//   primary: 'red',
-//   secondary: '#ff7200',
-//   tertiary: '#c7e5fc',
-//   neutral: '#f5f5f5',
-//   black: '#000000',
-//   grey: '#808080',
-//   white: '#ffffff',
-// }
 
 // const styles = computed(() => {
 //   const obj = {}
@@ -159,63 +100,3 @@ const white = '#ffffff'
 // console.log('Styles: ', styles.value)
 // console.log('Themes: ', themes.value)
 </script>
-
-<style>
-/* :root {
-  --primary: {{ primary }};
-  --secondary: {{ secondary }};
-  --tertiary: {{ tertiary }};
-  --neutral: {{ neutral }};
-  --black: {{ black }};
-  --grey: {{ grey }};
-  --white: {{ white }};
-} */
-
-/* :root {
-  --wrapper-bg: var(--white);
-  --pretitle-color: var(--secondary);
-  --title-color: var(--primary);
-  --body-color: var(--primary);
-  --btn-primary-bg: var(--secondary);
-  --btn-primary-text: var(--white);
-  --btn-secondary-bg: var(--primary);
-  --btn-secondary-text: var(--white);
-  --btn-simple-text: var(--primary);
-} */
-
-/* [data-theme='theme-1'] {
-  --wrapper-bg: var(--neutral);
-  --pretitle-color: var(--secondary);
-  --title-color: var(--primary);
-  --body-color: var(--primary);
-  --btn-primary-bg: var(--secondary);
-  --btn-primary-text: var(--white);
-  --btn-secondary-bg: var(--primary);
-  --btn-secondary-text: var(--white);
-  --btn-simple-text: var(--primary);
-} */
-
-/* [data-theme='theme-2'] {
-  --wrapper-bg: var(--primary);
-  --pretitle-color: var(--white);
-  --title-color: var(--white);
-  --body-color: var(--white);
-  --btn-primary-bg: var(--secondary);
-  --btn-primary-text: var(--white);
-  --btn-secondary-bg: var(--white);
-  --btn-secondary-text: var(--black);
-  --btn-simple-text: var(--white);
-} */
-
-/* [data-theme='theme-3'] {
-  --wrapper-bg: var(--secondary);
-  --pretitle-color: var(--white);
-  --title-color: var(--white);
-  --body-color: var(--white);
-  --btn-primary-bg: var(--white);
-  --btn-primary-text: var(--primary);
-  --btn-secondary-bg: var(--white);
-  --btn-secondary-text: var(--primary);
-  --btn-simple-text: var(--white);
-} */
-</style>
